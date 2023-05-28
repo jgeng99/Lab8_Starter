fea63e001d94490fb496c77b0b9c307825ca8fe4
Jianming <Georgegeng00@hotmail.com>
2023-05-28 15:09:31 -0700

    Finish lab08

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
 # Lab 8 - Starter
 
+## Name
+- Jianming Geng
+
 ### **1)** Where would you fit your automated tests in your Recipe project development pipeline? Select one of the following and explain why.
 
 1. Within a Github action that runs whenever code is pushed 
 2. Manually run them locally before pushing code
 3. Run them all after all development is completed
+
+- The answer is apparently 1. This is because automated tests integrated within a CI/CD pipeline (such as Github Actions) enable early detection of issues or bugs. They run on every code push, catching any regressions introduced by the new changes. In addition, by running tests on a dedicated CI server, you ensure that tests are run in a consistent environment. This can help prevent "worked on my machine" issues. Once it's set up, automated testing within a pipeline doesn't consume developer time, leaving them free to work on other tasks while tests run.
+
+### **2)** Would you use an end to end test to check if a function is returning the correct output? (yes/no)
+
+- No. End-to-end (E2E) tests are designed to test the entire application flow from start to finish, simulating a real user scenario. They are typically used to verify that the whole system, including all its components and integrations, works together as expected.
+
+### **3)** Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+- No, I would not use unit test to test the message feature. Although we can use unit test to test some independent features of the message, such as "when a message is sent, is it properly stored in the database?" or "can a message be successfully retrieved from the database?" However, unit tests have their limits and are not designed to test interactions between components or the behavior of the system as a whole.
+
+### **4)** Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+- Yes, testing the "max message length" feature of a messaging application is an ideal use case for a unit test. This feature has a clear, defined behavior that can be tested in isolation, which is the main characteristic of what should be unit tested.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,104 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
-// TODO - Part 2
+// Part 2
+
+// isPhoneNumber true
+test('test phone number 123-456-7891', () => {
+    expect(functions.isPhoneNumber("123-456-7891")).toBe(true);
+});
+
+// isPhoneNumber true
+test('test phone number 456-5677898', () => {
+    expect(functions.isPhoneNumber("456-5677898")).toBe(true);
+});
+
+// isPhoneNumber false
+test('test phone number 8583496271', () => {
+    expect(functions.isPhoneNumber("8583496271")).toBe(false);
+});
+
+// isPhoneNumber false
+test('test phone number jianming-geng', () => {
+    expect(functions.isPhoneNumber("jianming-geng")).toBe(false);
+});
+
+// isEmail true
+test('test isEmail jgeng@ucsd.edu', () => {
+    expect(functions.isEmail("jgeng@ucsd.edu")).toBe(true);
+});
+
+// isEmail true
+test('test isEmail jgeng99@ucb.cn', () => {
+    expect(functions.isEmail("jgeng99@ucb.cn")).toBe(true);
+});
+
+// isEmail false
+test('test isEmail email@china.hhhh', () => {
+    expect(functions.isEmail("email@china.hhhh")).toBe(false);
+});
+
+// isEmail false
+test('test isEmail email123@@ucsd.edu', () => {
+    expect(functions.isEmail("email123@@ucsd.edu")).toBe(false);
+});
+
+// isStrongPassword true
+test('test isStrongPassword sTrongPaS3', () => {
+    expect(functions.isStrongPassword("sTrongPaS3")).toBe(true);
+});
+
+// isStrongPassword true
+test('test isStrongPassword abcdEfh111222', () => {
+    expect(functions.isStrongPassword("abcdEfh111222")).toBe(true);
+});
+
+// isStrongPassword false
+test('test isStrongPassword AB', () => {
+    expect(functions.isStrongPassword("ab")).toBe(false);
+});
+
+// isStrongPassword false
+test('test isStrongPassword ASD@#$', () => {
+    expect(functions.isStrongPassword("ASD@#$")).toBe(false);
+});
+
+// isDate true
+test('test isDate 1/1/2000', () => {
+    expect(functions.isDate("1/1/2000")).toBe(true);
+});
+
+// isDate true
+test('test isDate 02/09/2001', () => {
+    expect(functions.isDate("02/09/2001")).toBe(true);
+});
+
+// isDate false
+test('test isDate 01/01/99', () => {
+    expect(functions.isDate("01/01/99")).toBe(false);
+});
+
+// isDate false
+test('test isDate 02/03/200Q', () => {
+    expect(functions.isDate("02/03/200Q")).toBe(false);
+});
+
+// isHexColor true
+test('test isHexColor 123', () => {
+    expect(functions.isHexColor("123")).toBe(true);
+});
+
+// isHexColor true
+test('test isHexColor ABC12f', () => {
+    expect(functions.isHexColor("ABC12f")).toBe(true);
+});
+
+// isHexColor false
+test('test isHexColor abdee', () => {
+    expect(functions.isHexColor("abdee")).toBe(false);
+});
+
+// isHexColor false
+test('test isHexColor Abg', () => {
+    expect(functions.isHexColor("Abg")).toBe(false);
+});
